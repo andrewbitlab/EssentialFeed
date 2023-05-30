044f848f7b2e8f5b33ecc668f0eabd1a28d07fc5
Andrzej Kwiatkowski <andrzej.kwiatkowski@redge.com>
2023-05-30 18:13:15 +0200

    Create the FeedLoader protocol boundary

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Andrzej Kwiatkowski on 30/05/2023.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
