772b9cc83d7ff9fe452bf5c550dd4548c0db374d
Andrzej Kwiatkowski <andrzej.kwiatkowski@redge.com>
2023-06-26 18:03:01 +0200

    Move HTTPClientSpy to separate file

---
+++ EssentialFeedTests/Feed API/Helpers/HTTPClientSpy.swift
@@ -0,0 +1,38 @@
+//
+//  HTTPClientSpy.swift
+//  EssentialFeedTests
+//
+//  Created by Andrzej Kwiatkowski on 24/06/2023.
+//
+
+import XCTest
+import Foundation
+import EssentialFeed
+
+class HTTPClientSpy: HTTPClient {
+    private var messages = [(url: URL, completion: (HTTPClienResult) -> Void)]()
+    var requestedURLs: [URL] {
+        return messages.map { $0.url }
+    }
+    
+    func get(from url: URL, completion: @escaping (HTTPClienResult) -> Void) {
+        messages.append((url, completion))
+    }
+    
+    func complete(with error: Error, at index: Int = 0) {
+        messages[index].completion(.failure(error))
+    }
+    
+    func complete(withStatusCode code: Int, data: Data, at index: Int = 0, file: StaticString = #filePath, line: UInt = #line) {
+        guard requestedURLs.count > index else {
+            return XCTFail("Can't complete request never made", file: file, line: line)
+        }
+        
+        let response = HTTPURLResponse(
+            url: requestedURLs[index],
+            statusCode: code,
+            httpVersion: nil,
+            headerFields: nil)!
+        messages[index].completion(.success(data, response))
+    }
+}
