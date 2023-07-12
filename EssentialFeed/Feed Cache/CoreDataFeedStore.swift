a895228a9852a9d56fbb58d04f1372c64aa18ab2
Andrzej Kwiatkowski <andrzej.kwiatkowski@redge.com>
2023-07-12 18:04:43 +0200

    CoreDataFeedStore.deleteCachedFeed() delivers no error on empty cache

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -47,7 +47,9 @@ public final class CoreDataFeedStore: FeedStore {
         }
     }
 
-    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {}
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        completion(nil)
+    }
 }
 
 private extension NSPersistentContainer {
